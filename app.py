72979ef09c192f5dc6f9f454ec1d7fe608c7bbef
Winnie Chou <winnie.chou.mb@gmail.com>
2020-12-03 18:27:06 -0600

    code review comments and edits for stage nine

--- app.py
+++ app.py
@@ -8,38 +8,46 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
-
+RESPONSE_STORAGE_KEY = 'responses'
 
 @app.route('/')
 def show_start():
     """ Shows survey home page for user to select 'start' """
 
-    return render_template('survey_start.html', title=survey.title,
-                           instructions=survey.instructions)
+    session[RESPONSE_STORAGE_KEY] = []
+
+    return render_template('survey_start.html', survey=survey)
 
 
 @app.route('/begin', methods=["POST"])
 def start_survey():
     """ Redirects to first question page  """
 
-    session['responses'] = []
-  
-    return redirect(f"/questions/{len(session['responses'])}")
+    session["start_key"] = True
+
+    return redirect('/questions/0')
 
 
 @app.route('/questions/<int:question_num>')
 def show_question(question_num):
     """ Display question page to user """
 
-    if len(session['responses']) == len(survey.questions):
+    if session.get("start_key"):
+        responses = session[RESPONSE_STORAGE_KEY]
+    else:
+        return redirect('/')
+
+    if len(responses) == len(survey.questions):
+        flash("You're done!")
         return redirect('/thankyou')
 
-    if question_num != len(session['responses']):
-        return redirect(f"/questions/{len(session['responses'])}")
-    curr_question = survey.questions[question_num].question
-    curr_choices = survey.questions[question_num].choices
-    return render_template('question.html', question=curr_question,
-                           choices=curr_choices)
+    if question_num != len(responses):
+        flash("Invalid question!")
+        return redirect(f"/questions/{len(responses)}")
+
+    curr_question = survey.questions[question_num]
+
+    return render_template('question.html', question=curr_question)
 
 
 @app.route('/answer', methods=["POST"])
@@ -48,7 +56,7 @@ def grab_answer():
         Redirects to next question """
 
     curr_answer = request.form["answer"]
-    responses = session['responses']
+    responses = session[RESPONSE_STORAGE_KEY]
     responses.append(curr_answer)
     session['reponses'] = responses
 
